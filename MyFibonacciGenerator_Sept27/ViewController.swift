d940611cfc71cede38eedf95edbef2177c0e5919
Will Smith <yokoref@gmail.com>
2015-09-27 17:07:07 -0500

    Initial Commit

---
+++ MyFibonacciGenerator_Sept27/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFibonacciGenerator_Sept27
+//
+//  Created by William Smith on 9/27/15.
+//  Copyright © 2015 William Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
