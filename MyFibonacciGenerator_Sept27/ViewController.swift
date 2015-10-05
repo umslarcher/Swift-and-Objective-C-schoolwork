5eba7f3ea735c00953e809bac1a446c802914b4c
Will Smith <yokoref@gmail.com>
2015-10-05 03:02:46 -0500

    Fib Generator test to git

--- MyFibonacciGenerator_Sept27/ViewController.swift
+++ MyFibonacciGenerator_Sept27/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var ResultLabel: UILabel!
     
@@ -17,9 +19,13 @@ class ViewController: UIViewController {
     @IBAction func GenFibButton(sender: AnyObject) {
         
         // Start function generateFibinacci()
+        
+        
         ResultLabel.text = generateFibinacci()
-
+        
     }
+    
+
     
     @IBAction func ResetButton(sender: AnyObject) {
         
@@ -30,49 +36,87 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.UserInText.delegate = self;
     }
+    
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        resignFirstResponder()
+        
+    }
+    
+
+    
+    
+
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        //textField code
+        
+        textField.resignFirstResponder()  //if desired
+        ResultLabel.text = generateFibinacci()
+        return true
+    }
+    
+    
+    
+    func performAction() {
+        
+        //action events
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    
-    
     func generateFibinacci() -> String{
         
-        
-        var result = 0
-        var i = 0
-        var n1 = 0
-        var n2 = 1
-        let n3:Int? = Int(UserInText.text!)
-        var series = String(n1)
-        series += ", "
-        series += String(n2) + ", "
-        
-        
-        
-        //In class we used 3 dots ... for 1 through 10 but couldn't get to work with user entered variable
-        
-        for (i=1; i<=n3; i++){
-            result = n1+n2
-            series += String(result)
+       
+            var result = 0
+            var i = 0
+            var n1 = 0
+            var n2 = 1
+            let n3:Int? = Int(UserInText.text!)
+            var series = String(n1)
             series += ", "
-            n1 = n2
-            n2 = result
+            series += String(n2) + ", "
+        
+        
+        
+//            var n4 = n3;
+        
+//            if(n4 = 1...45){
+//                ResultLabel.text = "Please Enter an integer from 1 to 45!";
+//                
+//            }
+//
+        
+        
+            
+            //In class we used 3 dots ... for 1 through 10 but couldn't get to work with user entered variable
+            
+            for (i=1; i<=n3; i++){
+                result = n1+n2
+                series += String(result)
+                series += ", "
+                n1 = n2
+                n2 = result
+                
+            }
+            
+            series = series.substringToIndex(series.endIndex.predecessor())
+            
+            return series;
             
             
         }
         
-        series = series.substringToIndex(series.endIndex.predecessor())
         
-        return series;
-        
-        
-        
-        
-    }
-
+    
 }
+
+        
+    
 
